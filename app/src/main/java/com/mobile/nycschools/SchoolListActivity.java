81b96e82982d81939023fa7395a604c90b089561
Steven <bonillasc@gmail.com>
2018-10-30 00:45:10 -0400

    started project with realm file

--- app/src/main/java/com/mobile/nycschools/SchoolListActivity.java
+++ app/src/main/java/com/mobile/nycschools/SchoolListActivity.java
@@ -3,11 +3,18 @@ package com.mobile.nycschools;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class SchoolListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_school_list);
+
+        // init the Realm file
+        Realm.init(this);
+        //RealmConfiguration realmConfig = new RealmConfiguration.Builder()
     }
 }
